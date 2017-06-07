315b7fe7f90731d13fc59baf3b50d91052b5cd7a
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-06-07 12:00:53 -0400

    Add inheritance examples

--- prototypes.js
+++ prototypes.js
@@ -1,29 +1,3 @@
-function Item(name, price){
-  this.name = name;
-  this.price = price;
-}
-
-Item.prototype.calculatePrice = function() {
-  if (this.name === 'fruit') {
-    return this.price * 0.95;
-  }
-
-  else {
-    return this.price;
-  }
-};
-
-var ball = new Item('soccer ball', 15);
-console.log( ball.calculatePrice() );
-// => 15
-
-var fruit = new Item('fruit', 10);
-console.log( fruit.calculatePrice() );
-// => 9.5
-
-
-
-
 function Animal (nameParam, ownerParam, personalityParam) {
   this.name = nameParam;
   this.owner = ownerParam;
@@ -47,3 +21,47 @@ var pet5 = new Animal('Widget', 'Nick', 'Bratty');
 
 console.log( pet1.showOwnerName() );
 console.log( pet5.episode() );
+
+
+function Dog (nameParam, ownerParam, personalityParam) {
+  // Apply the Animal constructor to the new object being created
+  Animal.call(this, nameParam, ownerParam, personalityParam);
+  this.breed = 'Collie';
+}
+
+// Create a protype for Dog that has Animal.prototype in the "chain"
+// (so you can use the functions of Animal.prototype)
+Dog.prototype = Object.create(Animal.prototype);
+
+// Dog-specific functions AFTER that ^^^
+
+
+var myDog = new Dog('Lassy', 'That Kid', 'Heroic');
+
+console.log( myDog.episode() );
+
+
+
+
+function Item(name, price){
+  this.name = name;
+  this.price = price;
+}
+
+Item.prototype.calculatePrice = function() {
+  if (this.name === 'fruit') {
+    return this.price * 0.95;
+  }
+
+  else {
+    return this.price;
+  }
+};
+
+var ball = new Item('soccer ball', 15);
+console.log( ball.calculatePrice() );
+// => 15
+
+var fruit = new Item('fruit', 10);
+console.log( fruit.calculatePrice() );
+// => 9.5
